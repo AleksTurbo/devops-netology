12eebef8494897fee069cae1fb34b94799c21047
AleksTurbo <AleksTurbo@yandex.ru>
2023-07-19 21:16:19 +0300

    upd 15.2

--- clopro/trfrm2/outputs.tf
+++ clopro/trfrm2/outputs.tf
@@ -1,7 +1,23 @@
-output "external_load_balancer_ip" {
-  value = yandex_lb_network_load_balancer.load-balancer-1.listener.*.external_address_spec[0].*.address[0]
+output "test-image-1-url" {
+  value = "https://${yandex_storage_bucket.netology-test-bucket.bucket_domain_name}/AlksTrbLogo0.png"
 }
 
-output "bucket_domain_name" {
-  value = "http://${yandex_storage_bucket.netology-bucket.bucket_domain_name}/test_pic.jpg"
+output "test-image-2-url" {
+  value = "https://${yandex_storage_bucket.netology-test-bucket.bucket_domain_name}/AlksTrbLogo0.jpg"
+}
+
+output "instance-0-ip" {
+  value = yandex_compute_instance_group.ig.instances.0.network_interface.0.ip_address
+}
+
+output "instance-1-ip" {
+  value = yandex_compute_instance_group.ig.instances.0.network_interface.0.ip_address
+}
+
+output "instance-2-ip" {
+  value = yandex_compute_instance_group.ig.instances.0.network_interface.0.ip_address
+}
+
+output "alb_target_group" {
+  value = yandex_alb_target_group.netology-tg.id
 }
